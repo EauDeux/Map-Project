f133c1e2a3eaf921a89e19c518605aaa659239e1
Aude K <aude.kski@gmail.com>
2020-11-22 15:18:38 +0100

    add i18n (translation)

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,21 +3,29 @@ import React from 'react'
 import { BrowserRouter as Router, Link } from 'react-router-dom'
 import Routes from '../../config/router'
 
+import { useTranslation } from 'react-i18next'
+
 import { StyledNav, StyledUl, StyledLink } from './Nav.styled'
+import Language from '../language'
 
 // eslint-disable-next-line no-unused-vars
 const Header = props => {
+  const { t } = useTranslation()
+
   return (
     <header>
       <Router>
         <StyledNav>
           <StyledUl>
             <StyledLink>
-              <Link to='/'>Home</Link>
+              <Link to='/'>{t('nav.home')}</Link>
             </StyledLink>
             <StyledLink>
-              <Link to='/about'>A propos de nous</Link>
+              <Link to='/about'>{t('nav.about')}</Link>
             </StyledLink>
+            {/* <StyledLink> */}
+            <Language />
+            {/* </StyledLink> */}
           </StyledUl>
         </StyledNav>
 
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
+
 import { ThemeProvider } from 'styled-components'
-
 import Header from '../components/header'
-
 import { theme } from '../config/theme'
+import '../config/translations'
 
 function App() {
   return (
