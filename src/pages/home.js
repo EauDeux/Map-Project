647bf229308d5268aaa0e00fc91bad0503654987
mamy98 <mamy.ndiaye98@gmail.com>
2020-11-25 21:41:23 +0100

    add map

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import ReactTooltip from 'react-tooltip'
+import './home.css'
 
-const Home = () => {
+import MapChart from '../components/MapChart.js'
+
+function Home() {
+  const [content, setContent] = useState('')
   return (
     <div>
-      <h1>Home</h1>
+      <MapChart setTooltipContent={setContent} />
+      <ReactTooltip>{content}</ReactTooltip>
     </div>
   )
 }
-
+const rootElement = document.getElementById('root')
+ReactDOM.render(<Home />, rootElement)
 export default Home
