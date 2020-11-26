6be3716bd89ec959ba0bcb2fc41775ec56020495
Aude K <aude.kski@gmail.com>
2020-11-26 15:08:09 +0100

    fix failed deploy

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,35 +1,19 @@
-<<<<<<< HEAD
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import ReactTooltip from 'react-tooltip'
+import './home.css'
+import MapChart from '../components/map/MapChart.js'
 
 const Home = () => {
   const { t } = useTranslation()
-  return (
-    <div>
-      <h1>{t('nav.home')}</h1>
-    </div>
-  )
-}
-
-export default Home
-=======
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
-import ReactTooltip from 'react-tooltip'
-import './home.css'
-
-import MapChart from '../components/map/MapChart.js'
-
-function Home() {
   const [content, setContent] = useState('')
   return (
     <div>
+      <h1>{t('nav.home')}</h1>
       <MapChart setTooltipContent={setContent} />
       <ReactTooltip>{content}</ReactTooltip>
     </div>
   )
 }
-const rootElement = document.getElementById('root')
-ReactDOM.render(<Home />, rootElement)
+
 export default Home
->>>>>>> aminata
